369f807479da93b7793587016b19439b917c061e
Chonglin Zhang <zhangchonglin@gmail.com>
2023-07-14 12:35:17 -0500

    add simulation case to be run on GPU partition

---
+++ 2D_flow_over_cylinder_GPU/README.md
@@ -0,0 +1,6 @@
+2D flow over circle case with adaptive mesh refinement
+ - This case uses the GPU
+ - `run_talon.sh` is the job submission script for UND's Talon HPC cluster;
+   - To submit job, use `sbatch run_talon.sh`.
+    
+
---
+++ 2D_flow_over_cylinder_GPU/run_talon.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+##### Partition to use
+#SBATCH --partition=talon
+#SBATCH -t 1:00:00
+##### Number of nodes
+#SBATCH --nodes=1
+##### specify to use GPU partition
+#SBATCH --partition=talon-gpu32
+##### use 2 GPUs
+#SBATCH --gres=gpu:2
+##### Number of tasks per node
+#SBATCH --ntasks-per-node=2
+#SBATCH --job-name=dsmc_2d_test
+#SBATCH --chdir=./
+##### Output file. This and the error file are the first two things we check when we are troubleshooting issues with your job. 
+#SBATCH -o dsmc_2d_flow_%j.out
+##### Error file. This and the output file are the first two things we check when we are troubleshooting issues with your job.
+#SBATCH -e dsmc_2d_flow_%j.err
+
+# Changes working directory to the directory where this script is submitted from
+printf 'Changing to the working directory: %s\n\n' "$SLURM_SUBMIT_DIR"
+cd $SLURM_SUBMIT_DIR
+
+# Load Necessary Modules -- Add whatever modules you need to run your program
+printf 'Loading modules\n'
+module load slurm
+module load mpich/ge/gcc/64/3.3.2
+module load cuda11.7/toolkit/11.7.1
+
+echo 'number of mpi ranks:' ${SLURM_NTASKS}
+# Determine the job host names and write a hosts file
+srun -n $SLURM_NTASKS hostname | sort -u > $SLURM_JOB_ID.hosts
+
+# Run program using mpirun using GPU
+mpirun -np $SLURM_NTASKS ./spa_kokkos_cuda -in in.circle -k on g 2 -sf kk
+
+# Remove Hosts file
+rm ${SLURM_JOB_ID}.hosts
