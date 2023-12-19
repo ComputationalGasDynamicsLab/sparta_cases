e430dff509a6eb6415d0a2a157d1b319eca0fae0
Chonglin Zhang <zhangchonglin@gmail.com>
2023-12-19 01:52:36 -0600

    update grid conversion script

--- 2D_flow_over_flatplate/paraview_visualization_scripts/grid_conversion.sh
+++ 2D_flow_over_flatplate/paraview_visualization_scripts/grid_conversion.sh
@@ -1,1 +1,2 @@
-pvpython grid2paraview.py grid_description.txt flow_result -r ../grid.*.data
+#pvpython grid2paraview.py grid_description.txt flow_result -r ../grid.*.data
+/home/chonglin.zhang/dsmc/paraview/ParaView_5.11.2/bin/pvpython grid2paraview.py grid_description.txt flow_result -r ../grid.*.data
