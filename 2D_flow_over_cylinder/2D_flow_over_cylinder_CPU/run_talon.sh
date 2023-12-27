40585a2cfcab251142eb862d99d73ab1d4500344
Chonglin Zhang <zhangchonglin@gmail.com>
2023-12-27 13:27:56 -0600

    update CPU run script

--- 2D_flow_over_cylinder/2D_flow_over_cylinder_CPU/run_talon.sh
+++ 2D_flow_over_cylinder/2D_flow_over_cylinder_CPU/run_talon.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 ##### Partition to use
 #SBATCH --partition=talon
-#SBATCH -t 1:00:00
+#SBATCH -t 5:00:00
 ##### Number of nodes
 #SBATCH --nodes=1
 ##### Number of tasks per node
+##### uses 36 CPUs
 #SBATCH --ntasks-per-node=36
-#SBATCH --job-name=dsmc_2d_test
+#SBATCH --job-name=dsmc_cylinder
 #SBATCH --chdir=./
-##### Output file. This and the error file are the first two things we check when we are troubleshooting issues with your job. 
+##### Output file.
 #SBATCH -o dsmc_2d_flow_%j.out
-##### Error file. This and the output file are the first two things we check when we are troubleshooting issues with your job.
-#SBATCH -e dsmc_2d_flow_%j.err
 
 # Changes working directory to the directory where this script is submitted from
 printf 'Changing to the working directory: %s\n\n' "$SLURM_SUBMIT_DIR"
@@ -21,13 +20,11 @@ cd $SLURM_SUBMIT_DIR
 printf 'Loading modules\n'
 module load slurm
 module load mpich/ge/gcc/64/3.3.2
+num_mpi_ranks=$(( SLURM_NNODES * $SLURM_NTASKS_PER_NODE ))
 
-echo 'number of mpi ranks:' ${SLURM_NTASKS}
-# Determine the job host names and write a hosts file
-srun -n $SLURM_NTASKS hostname | sort -u > $SLURM_JOB_ID.hosts
+echo 'number of node:' ${SLURM_NNODES}
+echo 'number of tasks per node:' ${SLURM_NTASKS_PER_NODE}
+echo 'number of mpi ranks:' ${num_mpi_ranks}
 
 # Run program using mpirun
-mpirun -np $SLURM_NTASKS ./spa_talon -in in.circle
-
-# Remove Hosts file
-rm ${SLURM_JOB_ID}.hosts
+mpirun -np $num_mpi_ranks ./spa_talon -in in.circle
