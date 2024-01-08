e11b4f29f0f8cc2f4ee4cc610cdec83cb492e26a
Chonglin Zhang <zhangchonglin@gmail.com>
2024-01-08 13:37:39 -0600

    add talon paraview scripts

---
+++ 3D_flow_over_Orion/3D_flow_over_Orion_fine_mesh/paraview_visualization_scripts/surface_conversion_talon.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+##### Partition to use
+#SBATCH --partition=talon-short
+#SBATCH -t 0:30:00
+##### Number of nodes
+#SBATCH --nodes=1
+##### Number of tasks per node
+##### uses 36 CPUs
+#SBATCH --ntasks-per-node=1
+#SBATCH --job-name=dsmc_orion
+#SBATCH --chdir=./
+##### Output file.
+#SBATCH -o dsmc_3d_flow_%j.out
+
+# Changes working directory to the directory where this script is submitted from
+printf 'Changing to the working directory: %s\n\n' "$SLURM_SUBMIT_DIR"
+cd $SLURM_SUBMIT_DIR
+
+# Load Necessary Modules -- Add whatever modules you need to run your program
+printf 'Loading modules\n'
+module load slurm
+module load mpich/ge/gcc/64/3.3.2
+num_mpi_ranks=$(( SLURM_NNODES * $SLURM_NTASKS_PER_NODE ))
+
+echo 'number of node:' ${SLURM_NNODES}
+echo 'number of tasks per node:' ${SLURM_NTASKS_PER_NODE}
+echo 'number of mpi ranks:' ${num_mpi_ranks}
+
+# Run program using mpirun
+# process grid files to be used in ParaView
+mpirun -np $num_mpi_ranks  /home/chonglin.zhang/dsmc/paraview/ParaView_5.11.2/bin/pvpython \
+surf2paraview.py ../sdata.orion surface_result -r ../surface.*.data
