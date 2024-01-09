c10f996de0e317158428c25f6aeec4401c7f1b27
Chonglin Zhang <zhangchonglin@gmail.com>
2024-01-09 14:28:04 -0600

    update paraview scripts for fine mesh case

--- 3D_flow_over_Orion/3D_flow_over_Orion_fine_mesh/paraview_visualization_scripts/grid_conversion.sh
+++
@@ -1,1 +0,0 @@
-pvpython grid2paraview.py grid_description.txt flow_result -r ../grid.*.data
---
+++ 3D_flow_over_Orion/3D_flow_over_Orion_fine_mesh/paraview_visualization_scripts/surface_conversion_centos.sh
@@ -0,0 +1,2 @@
+# process surface files to be used in ParaView
+/hdd1/paraview/ParaView-5.11.2-MPI-Linux-Python3.9-x86_64/bin/pvpython surf2paraview.py ../sdata.orion surface_result -r ../surface.*.data
