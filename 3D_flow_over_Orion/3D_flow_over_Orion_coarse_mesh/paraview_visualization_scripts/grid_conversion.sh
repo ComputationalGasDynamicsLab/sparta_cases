3900afdc24b56b6e7433c8d0ed409cc1c8b891f9
Chonglin Zhang <zhangchonglin@gmail.com>
2024-01-09 14:26:44 -0600

    add paraview scripts to coarse mesh case

--- 3D_flow_over_Orion/3D_flow_over_Orion_coarse_mesh/paraview_visualization_scripts/grid_conversion.sh
+++
@@ -1,1 +0,0 @@
-/home/chonglin.zhang/dsmc/paraview/ParaView_5.11.2/bin/pvpython grid2paraview.py grid_description.txt flow_result -r ../grid.*.data
---
+++ 3D_flow_over_Orion/3D_flow_over_Orion_coarse_mesh/paraview_visualization_scripts/grid_conversion_centos.sh
@@ -0,0 +1,2 @@
+# process grid files to be used in ParaView
+/hdd1/paraview/ParaView-5.11.2-MPI-Linux-Python3.9-x86_64/bin/pvpython grid2paraview.py grid_description.txt flow_result -r ../grid.*.data
