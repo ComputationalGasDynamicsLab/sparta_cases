79640ccf4c0920a3cfda4f32e2878e10b677c72c
Jalen  T <liquidsnake97@gmail.com>
2024-04-20 22:03:02 -0400

    Update install_sparta.sh

--- install_sparta.sh
+++ install_sparta.sh
@@ -1,10 +1,11 @@
 cd ~
-wget https://sjplimp.github.io//tars/sparta.tar.gz .
+wget "https://sjplimp.github.io//tars/sparta.tar.gz" .
 mkdir sparta_root && tar -xzf sparta.tar.gz -C sparta_root --strip-components 1
 mkdir ~/sparta_root/build_cpu && mkdir ~/sparta_root/build_gpu
-wget https://github.com/ComputationalGasDynamicsLab/sparta_build_scripts/blob/main/talon/build_sparta.sh -o ~/sparta_root/build_cpu/build_sparta.sh
-wget https://github.com/ComputationalGasDynamicsLab/sparta_build_scripts/blob/main/talon/build_sparta_kokkos.sh ~/sparta_root/build_gpu/build_sparta_kokkos.sh
+wget "https://github.com/ComputationalGasDynamicsLab/sparta_build_scripts/blob/main/talon/build_sparta.sh" -o ~/sparta_root/build_cpu/build_sparta.sh
+wget "https://github.com/ComputationalGasDynamicsLab/sparta_build_scripts/blob/main/talon/build_sparta_kokkos.sh" ~/sparta_root/build_gpu/build_sparta_kokkos.sh
 chmod +x ~/sparta_root/build_cpu/build_sparta.sh && chmox +x ~/sparta_root/build_cpu/build_sparta_kokkos.sh
 cd ~/sparta_root/build_cpu && ./build_sparta.sh && cd ../build_gpu && ./build_sparta_kkokos.sh
 
 cd ~ && rm -rf sparta.tar.gz
+cd ~
