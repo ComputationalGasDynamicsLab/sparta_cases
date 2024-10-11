e95a95c7d193f735b373e40ace80d1024370fb14
lbehrenb <lbehrenb@UNDCEMUP2-11L.ad.und.edu>
2024-10-11 10:52:59 -0500

    Add new msl case

---
+++ 2D_flow_over_msl/paraview_visualization_scripts/grid_conversion.sh
@@ -0,0 +1,3 @@
+#pvpython grid2paraview.py grid_description.txt flow_result -r ../grid.*.data
+/home/lbehrenb/dsmc/paraview/ParaView-5.12.1-MPI-Linux-Python3.10-x86_64/bin/pvpython grid2paraview.py grid_description.txt flow_result -r ../grid.*.data
+#/home/chonglin.zhang/dsmc/paraview/ParaView_5.11.2/bin/pvpython grid2paraview.py grid_description.txt flow_result -r ../grid.*.data
---
+++ 2D_flow_over_msl/run_centos_desktop_dump_file.sh
@@ -0,0 +1,1 @@
+mpirun -np 4 ./spa_kokkos_stokes -in in.msl_grid_surf_dump
